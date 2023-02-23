cd8d009be446d96f549dba0bac6e26b059feb9e2
Ivan Barrios <ivanbar8112003@gmail.com>
2023-02-23 02:11:11 -0300

    menu and coords Work!

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,26 +1,36 @@
 import { db } from "../firebase-config";
 import { getDocs, collection } from "firebase/firestore";
 import img from "../img/wiwImg.jpg";
+import { useState } from "react";
 
 const Game = () => {
-  const handleClick = () => {
-    //Append Menu
-  };
+  const [showMenu, setShowMenu] = useState(false);
+  const [clickCoords, setClickCoords] = useState({});
+  const [menuCoords, setMenuCoords] = useState({});
+
   const getClickPosition = (e) => {
+    setMenuCoords({ x: e.pageX, y: e.pageY });
     const rect = e.target.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    const realX = (x * 955) / e.target.clientWidth; // x coordinates
-    const realY = (y * 1536) / e.target.clientHeight; //y coordinates
+    const realX = (x * 955) / e.target.clientWidth;
+    const realY = (y * 1536) / e.target.clientHeight;
+    console.log(realX, realY);
     return {
       realX,
       realY,
     };
   };
 
+  const handleClick = (e) => {
+    setClickCoords(getClickPosition(e));
+    setShowMenu(!showMenu);
+  };
+
   const coordsCollection = collection(db, "charactersCoords");
 
   const handleMenuClick = (e) => {
+    setShowMenu(!showMenu);
     const getCharactersCoords = async () => {
       try {
         const data = await getDocs(coordsCollection);
@@ -38,7 +48,35 @@ const Game = () => {
   return (
     <div>
       <div className="flex justify-center bg-slate-900">
-        <img src={img} alt="Waldo Img" onClick={handleClick} />
+        <div>
+          <img src={img} alt="Waldo Img" onClick={handleClick} />
+          {showMenu ? (
+            <div
+              className="p-2 absolute bg-neutral-800 text-white flex flex-col w-32"
+              style={{ left: menuCoords.x, top: menuCoords.y }}
+            >
+              <p>Which is it?</p>
+              <button
+                className="hover:bg-neutral-600"
+                onClick={handleMenuClick}
+              >
+                Bear
+              </button>
+              <button
+                className="hover:bg-neutral-600"
+                onClick={handleMenuClick}
+              >
+                Monkey
+              </button>
+              <button
+                className="hover:bg-neutral-600"
+                onClick={handleMenuClick}
+              >
+                Seal
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
