665d9aa9d826349513030172c637105b012442ff
Ivan Barrios <ivanbar8112003@gmail.com>
2023-02-23 20:05:35 -0300

    Character color works

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,25 @@
 import { db } from "../firebase-config";
 import { getDocs, collection } from "firebase/firestore";
 import img from "../img/wiwImg.jpg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Game = ({ setBearColor, setMonkeyColor, setSealColor }) => {
   const [showMenu, setShowMenu] = useState(false);
   const [clickCoords, setClickCoords] = useState({});
   const [menuCoords, setMenuCoords] = useState({});
-  const [foundAnimal, setFoundAnimal] = useState("");
+  const [foundBear, setFoundBear] = useState(false);
+  const [foundMonkey, setFoundMonkey] = useState(false);
+  const [foundSeal, setFoundSeal] = useState(false);
+
+  useEffect(() => {
+    if (foundBear && foundMonkey && foundSeal) {
+      handleWin();
+    }
+  }, [foundBear, foundMonkey, foundSeal]);
+
+  const handleWin = () => {
+    //Handle Win
+  };
 
   const getClickPosition = (e) => {
     setMenuCoords({ x: e.pageX, y: e.pageY });
@@ -29,29 +41,48 @@ const Game = ({ setBearColor, setMonkeyColor, setSealColor }) => {
 
   const coordsCollection = collection(db, "charactersCoords");
 
-  const handleMenuClick = async (e) => {
+  const handleMenuClick = async (animal) => {
     setShowMenu(!showMenu);
-    setFoundAnimal(e.target.innerText);
-    console.log(foundAnimal);
-    const id = `${e.target.innerText.toLowerCase()}Coords`;
+    const id = `${animal}Coords`;
     const charactersCoords = await getCharactersCoords(id);
     const verifyX = Math.abs(clickCoords.realX - charactersCoords.x) < 60;
     const verifyY = Math.abs(clickCoords.realY - charactersCoords.y) < 60;
     console.log(clickCoords.realX, clickCoords.realY);
+    changeCharColor(verifyX, verifyY, animal);
+  };
+
+  const changeCharColor = (verifyX, verifyY, animal) => {
     if (verifyX && verifyY) {
-      switch (foundAnimal) {
+      switch (animal) {
         case "bear":
           setBearColor("green");
+          setFoundBear(true);
           break;
         case "monkey":
           setMonkeyColor("green");
+          setFoundMonkey(true);
           break;
         case "seal":
           setSealColor("green");
+          setFoundSeal(true);
           break;
         default:
           console.log("Sorry, there was an error finding the animal");
-      } //logging in the first click... why?
+      }
+    } else {
+      switch (animal) {
+        case "bear":
+          setBearColor("red");
+          break;
+        case "monkey":
+          setMonkeyColor("red");
+          break;
+        case "seal":
+          setSealColor("red");
+          break;
+        default:
+          console.log("Sorry, there was an error finding the animal");
+      }
     }
   };
 
@@ -81,19 +112,19 @@ const Game = ({ setBearColor, setMonkeyColor, setSealColor }) => {
               <p>Which is it?</p>
               <button
                 className="hover:bg-neutral-600"
-                onClick={handleMenuClick}
+                onClick={() => handleMenuClick("bear")}
               >
                 Bear
               </button>
               <button
                 className="hover:bg-neutral-600"
-                onClick={handleMenuClick}
+                onClick={() => handleMenuClick("monkey")}
               >
                 Monkey
               </button>
               <button
                 className="hover:bg-neutral-600"
-                onClick={handleMenuClick}
+                onClick={() => handleMenuClick("seal")}
               >
                 Seal
               </button>
